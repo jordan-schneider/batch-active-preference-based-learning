af9063e9ba736a0074915d8aabab7c3e91cd96f5
jordan-schneider <jordan.jack.schneider@gmail.com>
2020-10-07 15:13:29 -0400

    Adds hot restarting to human collection. Switches from list of numpys to just numpy arrays. Separates out true_reward.npy and reward.npy.

--- demos.py
+++ demos.py
@@ -1,7 +1,7 @@
 import sys
 from functools import partial
 from pathlib import Path
-from typing import Any, Callable, List, Tuple
+from typing import Any, Callable, List, Optional, Tuple, Union
 
 import argh  # type: ignore
 import numpy as np
@@ -16,39 +16,49 @@ from simulation_utils import create_env, get_feedback, run_algo
 GetPrefFunc = Callable[[Driver, np.ndarray, np.ndarray], Tuple[np.ndarray, int]]
 
 
+def append(a: Optional[np.ndarray], b: Union[np.ndarray, int]) -> np.ndarray:
+    if a is None:
+        if isinstance(b, np.ndarray):
+            return b.reshape(1, *b.shape)
+        elif isinstance(b, int):
+            return np.ndarray(b).reshape((1,))
+    else:
+        if isinstance(b, np.ndarray):
+            return np.append(a, b.reshape((1, *b.shape)), axis=0)
+        elif isinstance(b, int):
+            return np.append(a, b)
+
+
 def elicit_preferences(
     a_inputs: np.ndarray,
     b_inputs: np.ndarray,
     simulation_object: Driver,
-    normals: List[np.ndarray],
-    preferences: List[int],
+    normals: Optional[np.ndarray],
+    preferences: Optional[np.ndarray],
     outdir: Path,
     get_preference: GetPrefFunc,
-) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+) -> Tuple[np.ndarray, np.ndarray]:
     for input_a, input_b in zip(a_inputs, b_inputs):
         normal, preference = get_preference(simulation_object, input_a, input_b)
-        normals.append(normal)
-        preferences.append(preference)
-        np.save(outdir / "normals", normals)
-        np.save(outdir / "preferences", preferences)
+        normals = append(normals, normal)
+        preferences = append(preferences, preference)
+        np.save(outdir / "normals.npy", normals)
+        np.save(outdir / "preferences.npy", preferences)
     return normals, preferences
 
 
 def update_inputs(
-    a_inputs: np.ndarray,
-    b_inputs: np.ndarray,
-    inputs: List[Tuple[np.ndarray, np.ndarray]],
-    outdir: Path,
-) -> List[Tuple[np.ndarray, np.ndarray]]:
+    a_inputs: np.ndarray, b_inputs: np.ndarray, inputs: np.ndarray, outdir: Path,
+) -> np.ndarray:
     """Adds a new pair of input trajectories (a_inputs, b_inputs) to the inputs list and saves it."""
-    inputs.append((a_inputs, b_inputs))
-    np.save(outdir / "inputs", inputs)
+    inputs = append(inputs, np.stack([a_inputs, b_inputs]))
+    np.save(outdir / "inputs.npy", inputs)
     return inputs
 
 
 def sample_inputs(
-    normals: List[np.ndarray],
-    preferences: List[int],
+    normals: np.ndarray,
+    preferences: np.ndarray,
     w_sampler,
     simulation_object: Driver,
     M: int,
@@ -63,21 +73,21 @@ def sample_inputs(
 
 
 def sample_weights(
-    normals: List[np.ndarray], preferences: List[int], w_sampler, M: int
+    normals: np.ndarray, preferences: np.ndarray, w_sampler, M: int
 ) -> np.ndarray:
     w_sampler.A = normals
-    w_sampler.y = np.array(preferences).reshape(-1, 1)
+    w_sampler.y = preferences.reshape(-1, 1)
     w_samples = w_sampler.sample(M)
     return w_samples
 
 
 def save_reward(
-    normals: List[np.ndarray], preferences: List[int], w_sampler, M: int, outdir: Path
+    normals: np.ndarray, preferences: np.ndarray, w_sampler, M: int, outdir: Path
 ):
     w_samples = sample_weights(normals, preferences, w_sampler, M)
     mean_weight = np.mean(w_samples, axis=0)
     normalized_mean_weight = mean_weight / np.linalg.norm(mean_weight)
-    np.save(outdir / "reward.npy", normalized_mean_weight)
+    np.save(outdir / "mean_reward.npy", normalized_mean_weight)
 
 
 def get_simulated_feedback(
@@ -128,6 +138,27 @@ def make_random_inputs(
     return a_inputs, b_inputs
 
 
+def load_true_reward(
+    true_reward: Tuple[float, float, float, float], outdir: Path
+) -> np.ndarray:
+    true_reward_path = outdir / "true_reward.npy"
+    if true_reward_path.is_file():
+        return np.load(true_reward_path)
+    else:
+        return normalize(np.array(true_reward))
+
+
+def load(outdir: Path, filename: str, overwrite: bool) -> Optional[np.ndarray]:
+    if overwrite:
+        return None
+
+    filepath = outdir / filename
+    if filepath.exists():
+        return np.load(filepath)
+
+    return None
+
+
 @arg("--true-reward", nargs=4, type=float)
 def batch(
     *,
@@ -138,6 +169,7 @@ def batch(
     simulate_human: bool = False,
     true_reward: Tuple[float, float, float, float] = random((4,)) * 2 - 1,
     fake_noise: bool = False,
+    overwrite: bool = False,
 ):
     if N % b != 0:
         print("N must be divisible to b")
@@ -146,11 +178,11 @@ def batch(
 
     get_prefs: GetPrefFunc
     if simulate_human:
-        reward = normalize(np.array(true_reward))
-        np.save(outdir / "reward", reward)
+        true_reward = load_true_reward(true_reward, outdir)
+        np.save(outdir / "true_reward.npy", true_reward)
 
         get_prefs = partial(
-            get_simulated_feedback, reward=reward, fake_noise=fake_noise
+            get_simulated_feedback, reward=true_reward, fake_noise=fake_noise
         )
     else:
         get_prefs = get_feedback
@@ -158,26 +190,49 @@ def batch(
     simulation_object = create_env("driver")
 
     w_sampler = Sampler(simulation_object.num_of_features)
-    normals: List[np.ndarray] = []
-    preferences: List[np.ndarray] = []
-    inputs: List[Tuple[np.ndarray, np.ndarray]] = []
-
-    a_inputs, b_inputs = make_random_inputs(simulation_object, b)
-    inputs = update_inputs(a_inputs, b_inputs, inputs, outdir)
-
-    normals, preference = elicit_preferences(
-        a_inputs, b_inputs, simulation_object, normals, preferences, outdir, get_prefs,
+    normals: np.ndarray = load(outdir, filename="normals.npy", overwrite=overwrite)
+    preferences: np.ndarray = load(
+        outdir, filename="preferences.npy", overwrite=overwrite
     )
+    inputs: np.ndarray = load(outdir, filename="inputs.npy", overwrite=overwrite)
+
+    if len(inputs) == 0:
+        # If this is the first iteration, make random inputs
+        a_inputs, b_inputs = make_random_inputs(simulation_object, b)
+        inputs = update_inputs(a_inputs, b_inputs, inputs, outdir)
+        normals, preferences = elicit_preferences(
+            a_inputs,
+            b_inputs,
+            simulation_object,
+            normals,
+            preferences,
+            outdir,
+            get_prefs,
+        )
+    elif len(inputs) * b > len(preferences):
+        # If we exit in the middle of a batch, we need to finish up the rest of the batch
+        last_index = len(preferences) % b
+        a_inputs, b_inputs = inputs[-1, :, last_index:]
+        assert len(a_inputs) == len(inputs) * b - len(preferences)
+        normals, preferences = elicit_preferences(
+            a_inputs,
+            b_inputs,
+            simulation_object,
+            normals,
+            preferences,
+            outdir,
+            get_prefs,
+        )
 
     try:
-        for _ in range(b, N, b):
+        for _ in range(len(inputs) * b, N, b):
             if not simulate_human:
                 print("Thinking...")
             a_inputs, b_inputs = sample_inputs(
                 normals, preferences, w_sampler, simulation_object, M, b, B
             )
-           inputs = update_inputs(a_inputs, b_inputs, inputs, outdir)
-            normals, preference = elicit_preferences(
+            inputs = update_inputs(a_inputs, b_inputs, inputs, outdir)
+            normals, preferences = elicit_preferences(
                 a_inputs,
                 b_inputs,
                 simulation_object,
