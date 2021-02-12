897d6f33e97be6c1bb4328a836958877d6e0cfa3
Jordan Schneider <jordan.jack.schneider@gmail.com>
2021-02-12 10:34:06 -0800

    I don't actually need this script on remote. It doesn't work there.

--- sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for host in hopper lovelace johnson; do
-  rsync -au $host:value-alignment-verification/data/ /home/joschnei/value-alignment-verification/data/
-done
-for host in hopper lovelace johnson; do
-  rsync -au /home/joschnei/value-alignment-verification/data/ $host:value-alignment-verification/data 
-done
