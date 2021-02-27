188e0b0ee43569f01dfc8e67053875a38c17230c
Jordan Schneider <jordan.jack.schneider@gmail.com>
2021-02-27 11:43:36 -0800

    Removes local driver env in favor of separate repo.

--- active/setup.py
+++ active/setup.py
@@ -1,20 +1,18 @@
 from distutils.core import setup
 from pathlib import Path
 
-
-def local_dep(package: str) -> str:
-    # TODO(joschnei): This requires the value-alignment-verification folder to be installed in the
-    # user's home directory, which is not necessarily the case. This should be fixed but I don't
-    # know how. Probably the best thing to do is just clean up the driver env and publish that
-    # separately, and then pull the remote version instead of this local hack.
-    return f"{package}@file://localhost/{Path.home()}/value-alignment-verification/{package}/"
-
-
 # TODO(joschnei): Add typing info
 
 setup(
     name="active",
     version="0.1",
     packages=["active",],
-    install_requires=["scipy", "numpy", local_dep("driver")],
+    install_requires=[
+        "scipy",
+        "numpy",
+        "driver @ git+https://github.com/jordan-schneider/driver-env.git#egg=driver",
+    ],
+    package_data = {
+        'active': ['py.typed'],
+    },
 )
--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 import os
 from distutils.core import setup
 
-
-def local_dep(package: str) -> str:
-    return f"{package}@file://localhost/{os.getcwd()}/{package}/"
-
-
 # TODO(joschnei): Publish driver code on separate github so I can speify a remote dependency here.
 setup(
     name="value-alignment-verification",
     packages=["active"],
-    install_requires=[local_dep("driver"),],
+    install_requires=[
+        "numpy",
+        "torch",
+        "fire",
+        "argh",
+        "scikit-learn",
+        "joblib",
+        "matplotlib",
+        "seaborn",
+        "scipy",
+        "TD3 @ git+https://github.com/jordan-schneider/TD3.git#egg=TD3"
+        "driver @ git+https://github.com/jordan-schneider/driver-env.git#egg=driver",
+    ],
+    package_data={"value-alignment-verification": ["py.typed"],},
 )
