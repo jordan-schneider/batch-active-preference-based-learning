142b6cf9be871fde8032d4d4d890127d846b92b4
Jordan Schneider <jordan.jack.schneider@gmail.com>
2021-04-10 19:15:57 -0700

    Moves parallel code back to main file, since that doesn't actually help.

--- run_tests.py
+++ run_tests.py
@@ -25,7 +25,6 @@ from TD3 import Td3, load_td3  # type: ignore
 
 from active.simulation_utils import TrajOptimizer, assert_normals, make_normals, orient_normals
 from equiv_utils import add_equiv_constraints, remove_equiv
-from parallel_garbage import get_opt_actions
 from policy import make_td3_paths, make_TD3_state
 from random_baseline import make_random_questions
 from testing_factory import TestFactory
@@ -599,6 +598,43 @@ def rewards_aligned(
 
         logging.debug("Alignment done")
     return alignment
+
+
+def get_opt_actions(
+    rewards: np.ndarray,
+    states: np.ndarray,
+    optim: TrajOptimizer,
+    parallel: Parallel,
+    action_shape: Tuple[int, ...] = (2,),
+) -> np.ndarray:
+    def f(
+        rewards: np.ndarray,
+        states: np.ndarray,
+        optim: TrajOptimizer,
+        action_shape: Tuple[int, ...] = (2,),
+    ):
+        batch_size = rewards.shape[0]
+        assert states.shape[0] == batch_size
+        opt_actions = np.empty((batch_size, *action_shape))
+        for i, (reward, state) in enumerate(zip(rewards, states)):
+            path = optim.make_opt_traj(reward, state).reshape(-1, *action_shape)
+            opt_actions[i] = path[0]
+
+        return opt_actions
+
+    input_batches = np.array_split(list(product(rewards, states)), parallel.n_jobs)
+
+    return np.concatenate(
+        parallel(
+            delayed(f)(
+                rewards=batch[:, 0],
+                states=batch[:, 1],
+                optim=optim,
+                action_shape=action_shape,
+            )
+            for batch in input_batches
+        )
+    ).reshape(len(rewards), len(states), *action_shape)
 
 
 # Simulated Experiment
